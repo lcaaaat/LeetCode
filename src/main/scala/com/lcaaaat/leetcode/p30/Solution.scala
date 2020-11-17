e606c6cdf99236db8cfe4ee425d8fd6cd19bdb4f
lcaaaat <0x4c4a59@gmail.com>
2020-11-17 22:33:29 +0800

    problem 30 ac

--- src/main/scala/com/lcaaaat/leetcode/p30/Solution.scala
+++ src/main/scala/com/lcaaaat/leetcode/p30/Solution.scala
@@ -1,67 +1,58 @@
 package com.lcaaaat.leetcode.p30
 
-import scala.collection.mutable.ArrayBuffer
-import scala.collection.mutable
-
 object Solution {
-  def recursive(
-      locations: mutable.Map[Int, ArrayBuffer[String]],
-      words: mutable.Map[String, Int],
-      startLocation: Int,
-      currentLocation: Int,
-      result: mutable.Set[Int]
-  ): Unit = {
-    if (words.isEmpty) {
-      result += startLocation
-      return
-    }
-    if (!locations.contains(currentLocation)) {
-      return
-    }
-    locations(currentLocation).foreach { word =>
-      if (words.contains(word)) {
-        words(word) = words(word) - 1
-        if (words(word) == 0) {
-          words.remove(word)
-        }
-        recursive(locations, words, startLocation, currentLocation + word.length(), result)
-        if (words.contains(word)) {
-          words(word) = words(word) + 1
-        } else {
-          words(word) = 1
-        }
-      }
-    }
-  }
+  import scala.collection.mutable.ArrayBuffer
+  import scala.collection.mutable
 
   def findSubstring(s: String, words: Array[String]): List[Int] = {
-    val locations = mutable.Map[Int, ArrayBuffer[String]]()
-    val allWords = mutable.Map[String, Int]()
-    words.foreach { word =>
-      if (allWords.contains(word)) {
-        allWords(word) = allWords(word) + 1
-      } else {
-        allWords(word) = 1
-      }
-      var index = s.indexOf(word)
-      while (index != -1) {
-        if (!locations.contains(index)) {
-          locations(index) = ArrayBuffer[String]()
+    if (s != null && s.length > 0 && words != null && words.size > 0) {
+      val wordMap = mutable.Map[String, Int]()
+      words.foreach { word =>
+        if (wordMap.contains(word)) {
+          wordMap(word) = wordMap(word) + 1
+        } else {
+          wordMap(word) = 1
         }
-        locations(index) += word
-        index = s.indexOf(word, index + 1)
       }
-    }
 
-    val ret = mutable.Set[Int]()
-    for (i <- 0 until s.length()) {
-      recursive(locations, allWords, i, i, ret)
+      val ret = ArrayBuffer[Int]()
+      val length = words(0).length
+      for (i <- 0 until length) {
+        var left = i
+        var right = i
+        var count = 0
+        val tmp = mutable.Map[String, Int]()
+        while (right + length <= s.length()) {
+          val word = s.substring(right, right + length);
+          if (tmp.contains(word)) {
+            tmp(word) = tmp(word) + 1
+          } else {
+            tmp(word) = 1
+          }
+          if (!wordMap.contains(word)) {
+            wordMap(word) = 0
+          }
+          right += length
+          count += 1
+          while (tmp(word) > wordMap(word)) {
+            val leftWord = s.substring(left, left + length)
+            count -= 1
+            tmp(leftWord) = tmp(leftWord) - 1
+            left += length
+          }
+          if (count == words.size) {
+             ret += left
+          }
+        }
+      }
+      ret.toList
+    } else {
+      List[Int]()
     }
-    ret.toList
   }
 
   def main(args: Array[String]): Unit = {
-    val s = "barfoothefoobarman"
+    val s = "barfoobarthefoobarman"
     val words = Array[String]("foo", "bar")
     println(findSubstring(s, words))
   }
